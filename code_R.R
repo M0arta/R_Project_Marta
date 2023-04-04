23d0ebe56113849553e2c9cc725282aa6dda36af
Marta Bernardi <97482458+M0arta@users.noreply.github.com>
2023-04-04 18:22:19 +0200

    new code running

--- code_R.R
+++ code_R.R
@@ -1,4 +1,5 @@
-
+library(stringr)
+library(viridis)
 library(knitr)
 library(readr)
 library(sf)
@@ -6,8 +7,7 @@ library(readr)
 library(ggthemes)
 library(dplyr)
 library(ggplot2)
-
-setwd("C:/Users/Bernardi_Marta/Documents/Geo_data")
+setwd("C:/Users/Bernardi_Marta/Downloads/Zambia")
 
 Zambia_data <- read_csv("645.csv.gz")
 
@@ -35,8 +35,8 @@ mobile_access = read.csv("Zambia_data.csv")
 mobile_access_complete <- mobile_access[complete.cases(mobile_access[c("lat", "lon")]),]
 mobile_access_sf <- st_as_sf(mobile_access_complete, coords = c("lat", "lon"))
 st_crs(mobile_access_sf) <- 4326
-st_write(mobile_access_sf, "thcell.shp")
-mobile_access_shp <- st_read("thcell.shp")
+st_write(mobile_access_sf, "ycell.shp", delete_dsn = TRUE)
+mobile_access_shp <- st_read("ycell.shp")
 
 
 #Open district geographies and assign crs
@@ -46,11 +46,13 @@ st_crs(gred_shp) <- 4326
 
 #Try to plot data before merging to see how they look like, there should be a series of data points for each column but with no spatial distribution because I still need to asign it
 
-plot(mobile_access_shp)
+#plot(mobile_access_shp)
+#pdf("mobile_access_shp.png")
 
 #plot the shape of Zambia that I would like to put together 
 
-plot(gred_shp)
+#plot(gred_shp)
+#pdf("gred_shp.png")
 
 #Keep only useful columns in mobile phone data 
 
@@ -59,11 +61,11 @@ mobile_access_selected <- mobile_access_shp |>
 
 #Plot to see how the variable of interest look like when all the years are taken into consideration together 
 
-ggplot() + 
-  geom_sf(data = mobile_access_selected, aes(fill = CID)) +
-  scale_fill_viridis_c() +
-  theme_dark()
-
+#ggplot() + 
+#geom_sf(data = mobile_access_selected, aes(fill = CID)) +
+#scale_fill_viridis_c() +
+#theme_dark()
+#ggsave("presence_cells.png")
 
 #Slice year by year to decrease computational request and assign treatment year by year, to slice use the time the data were "Created"
 #the timestamp is a Unix timestamp representing the date and time as the number of seconds that have elapsed since January 1, 1970 at 00:00:00 UTC
@@ -95,8 +97,8 @@ mobile_2016 <- read_csv("zambia_mobile_2016.csv") #over 5000 observations so I'm
 mobile_2016_complete <- mobile_2016[complete.cases(mobile_2016[c("lat", "lon")]),]
 mobile_2016_sf <- st_as_sf(mobile_2016_complete, coords = c("lat", "lon"))
 st_crs(mobile_2016_sf) <- 4326
-st_write(mobile_2016_sf, "2016.shp")
-mobile_2016_shp <- st_read("2016.shp")
+st_write(mobile_2016_sf, "y2016.shp", delete_dsn = TRUE)
+mobile_2016_shp <- st_read("y2016.shp")
 
 
 # check the CRS of your spatial objects
@@ -109,113 +111,183 @@ mobile_2016_transformed <- st_transform(mobile_2016_shp, st_crs(gred_shp))
 ### Join the shape of the electoral districts with the cells in 2016 
 
 joined_sf <- st_join(gred_shp, mobile_2016_transformed, join = st_intersects)
-
+joined_sf <- na.omit(joined_sf)
 
 ### write joined data 2016 into a shape file and then use the normal plot to see how the different varibles look like in the space
-st_write(joined_sf, "joined_2016.shp")
+st_write(joined_sf, "yjoined_2016.shp", delete_dsn = TRUE)
 
-plot(joined_sf)
+#plot(joined_sf)
+#pdf("joined_sf.png")
+kable(head(joined_sf))
+
+### Rename variables in th merged dataset ad keep only usefull ones
+
+names(joined_sf)[names(joined_sf) == "cst_n"] <- "elec_district_name"
+names(joined_sf)[names(joined_sf) == "cst"] <- "elec_district_code"
+names(joined_sf)[names(joined_sf) == "Created"] <- "date"
+
+mobile_district <- joined_sf |> 
+  select(c('elec_district_name', 'geometry', 'elec_district_code', 'Range', 'CID', 'Rad_typ', 'date'))
 
 
 ## Assign treatment in the first way  using the presence/intensity  of mobile towers
 ###To do this create a new column with number of CID by electoral district 
 
-joined_sf <- joined_sf |>
-  group_by(cst_n) |>
+mobile_district <- mobile_district |>
+  group_by(elec_district_name) |>
   mutate(treat1 = n_distinct(CID))
+
 
 ###Plot treatment 1 having NA white 
 
-ggplot() + 
-  geom_sf(data = joined_sf, aes(fill = treat1)) +
-  scale_fill_viridis_c(na.value = "white", limits = c(0, 200), guide = guide_colorsteps(ncol = 50)) +
-  theme_dark() +
-  labs(fill = "Number of CIDs")
+#ggplot() + 
+# geom_sf(data = mobile_district, aes(fill = treat1)) +
+# scale_fill_viridis_c(na.value = "white", limits = c(0, 200), guide = guide_colorsteps(ncol = 50)) +
+#theme_dark() +
+#labs(fill = "Number of CIDs")
 
+#ggsave("treat1.png")
 
 ### Load data on elections outcome from the website and select Zambia data in 2016 election round
 
-load("~/Geo_data/elections.RData")
-
+load("clea_lc_20220908.RData") 
 Zambia_election <- clea_lc_20220908 |>
   filter(ctr_n == "Zambia")
 
 Zambia_election_2016 <- Zambia_election |>
   filter(yr == 2016)
+write.csv(Zambia_election_2016, "Zambia_election_2016.csv", row.names = FALSE)
 
-### Select only useful columns 
+### Select only useful columns and rename them 
 
 election_2016 <- Zambia_election_2016 |>
   select(c("cst_n", "pty_n", "pty", "pev1", "vv1", "to1", "cvs1", "pv1", "pvs1", "vot1", "ivv1"))
 
-### Write the name of the districts in aproper way before joining 
-election_2016$cst_n <- str_to_title(election_2016$cst_n)
+names(election_2016)[names(election_2016) == "cst_n"] <- "elec_district_name"
+names(election_2016)[names(election_2016) == "to1"] <- "voter_turn"
+names(election_2016)[names(election_2016) == "pty_n"] <- "party_name"
+names(election_2016)[names(election_2016) == "pty"] <- "party_code"
+names(election_2016)[names(election_2016) == "pev1"] <- "n_eligible_voters"
+names(election_2016)[names(election_2016) == "pv1"] <- "n_votes"
+names(election_2016)[names(election_2016) == "pvs1"] <- "party_vpte_share"
+names(election_2016)[names(election_2016) == "vv1"] <- "n_valid_votes"
+names(election_2016)[names(election_2016) == "ivv1"] <- "n_invalid_votes"
+names(election_2016)[names(election_2016) == "vot1"] <- "vote_cast"
+kable(head(election_2016))
+
+### Write the name of the districts in a proper way before joining 
+
+election_2016$elec_district_name <- str_to_title(election_2016$elec_district_name)
 
 ### Perform a left join between a spatial object and a normal database
-internet_election <- left_join( joined_sf, election_2016, by = "cst_n")
+internet_election <- left_join( joined_sf, election_2016, by = "elec_district_name")
 
 ### Look how potential outcome variables are distributed in space 
 
 ##Voters Turnout 
 
+#ggplot() + 
+# geom_sf(data = internet_election, aes(fill = internet_election$voter_turn)) +
+#scale_fill_viridis_c(na.value = "white", limits = c(0, 1), guide = guide_colorsteps(ncol = 50)) +
+#theme_dark() +
+#labs(fill = "Voters Turnout")
+#ggsave("outcome1.png")
+
+
+
+## Distribution of vote shares by parties to see main parties
+
+vote_share <- election_2016 |>
+  group_by(party_name) |>
+  summarize(total_votes = sum(n_votes, na.rm = TRUE))
+
+
+party_colors <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#999999", "#E6007E", "#980000", "#000080", "#E9A3C9", "#A9D18E", "#FFC8A3")
+
+
+ggplot(vote_share, aes(x = party_name, y = vote_share$total_votes, fill = party_colors)) + 
+  geom_bar(stat = "identity") +
+  labs(title = " Number of Votes by Party", x = "Party", y = "Number of votes") +
+  scale_fill_identity(guide = "legend", labels = vote_share$party_name) +
+  theme_dark() +
+  theme(axis.text.x = element_blank())
+ggsave("votesharebar.png")
+
+
+
+## Look at geographical distribution of the two main parties
+
+##  Vote share by district for National Restoration Party
+votes_by_district <- internet_election |>
+  filter(party_name == "National Restoration Party")|>
+  group_by(elec_district_name) %>% 
+  summarise(total_votes = sum(n_votes))
+
+
+ggplot(votes_by_district, aes(fill = total_votes, color = total_votes)) +
+  geom_sf(alpha = 0.5) +
+  scale_fill_gradient(low = "white", high = "blue") +
+  scale_color_gradient(low = "white", high = "blue") +
+  labs(title = "National Restoration Party - Votes distribution") +
+  theme_dark() +
+  guides(fill = guide_colorbar(title = "Number of Votes"), color = FALSE) +
+  coord_sf(crs = st_crs(votes_by_district), lims_method = "geometry_bbox")
+
+ggsave("nrp.png")
+## Vote share by district for Multi-Party Democracy
+add_district <- internet_election |>
+  filter(party_name == "Movement for Multi-Party Democracy")|>
+  group_by(elec_district_name)|> 
+  summarise(total_votes = sum(n_votes))
+
+
+ggplot(add_district, aes(fill = total_votes, color = total_votes)) +
+  geom_sf(alpha = 0.5) +
+  scale_fill_gradient(low = "white", high = "red") +
+  scale_color_gradient(low = "white", high = "red") +
+  labs(title = "Movement for Multi-Party Democracy - Votes distribution") +
+  theme_dark() +
+  guides(fill = guide_colorbar(title = "Number of Votes"), color = FALSE) +
+  coord_sf(crs = st_crs(add_district), lims_method = "geometry_bbox")
+
+ggsave("add.png")
+
+
+## Correlation CID and voters turnout 
+
+
+internet_election_clean <- na.omit(internet_election)
+ggplot(internet_election_clean, aes(x = treat1, y = voter_turn)) +
+  geom_point() +
+  stat_smooth(method = "lm", level = 0.90) +
+  labs(x = "Number of internet cells", y = "Voter Turnout")
+
+ggsave("lm.png")
+
+## Correlation CID and valid votes 
+
+ggplot(internet_election_clean, aes(x = treat1, y = n_valid_votes)) +
+  geom_point(color = "blue") +
+  stat_smooth(method = "lm", level = 0.90, color = "red", se = 0.90) +
+  labs(x = "Number of internet cells", y = "Number of Valid votes") +
+  theme_classic()
+ggsave("comp.png")
+
+
+## Assign treament by presence instead of intensity 
+
+
+internet_election <- internet_election |>
+  mutate(treat2 = ifelse(treat1 >= 1, 1, 0))
+
 ggplot() + 
-  geom_sf(data = internet_election, aes(fill = to1)) +
-  scale_fill_viridis_c(na.value = "white", limits = c(0, 1), guide = guide_colorsteps(ncol = 50)) +
-  theme_dark() +
-  labs(fill = "Voters Turnout")
+  geom_sf(data = internet_election, aes(fill = factor(treat2))) +
+  scale_fill_manual(values = c("0" = "white", "1" = "purple"), 
+                    na.value = "white",
+                    guide = guide_legend(title = "Presence of mobile towers")) +
+  theme_dark()
 
-## Plot correlation between presence of mobile cells and voters turnout 
-
-## first try in a  map 
-
-ggplot(internet_election) +
-  geom_sf(aes(fill = treat1, color = to1), alpha = 0.5, size = 0.2) +
-  scale_fill_viridis_c(name = "Number of Mobile cells ") +
-  scale_color_gradient(name = "Voters turnout") +
-  labs(title = "Correlation between Presence of Mobile cells and Voters Turnout") +
-  theme_dark() +
-  theme(plot.title = element_text(hjust = 0.5, size = 14),
-        legend.position = "bottom",
-        axis.title = element_text(size = 12),
-        axis.text = element_blank(),
-        legend.title = element_text(size = 12),
-        legend.text = element_text(size = 10))
-
-## not readable so go with normal scatterplot fitting a non linear trend 
-
-ggplot(internet_election, aes(x = treat1, y = to1)) +
-  geom_point(size = 2, alpha = 0.8, color = "#FF5733") +
-  geom_smooth(method = "loess", se = FALSE, color = "#0066CC", size = 1) +
-  labs(title = "Correlation between Presence of Mobile cells and Voters Turnout",
-       x = "Number of Mobile cells ", y = "Voter's turnout") +
-  theme_fivethirtyeight() +
-  theme(plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
-        axis.title = element_text(size = 14, face = "bold"),
-        axis.text = element_text(size = 12),
-        legend.position = "none")
-
-## not very well fitted by eyeballing 
-
-
-# Assign treatment in the second way using the range of the mobile towers 
+ggsave('treat2.png') 
 
 
 
-
-
-# Plot all Zambia treated and non-treated in the two versions 
-
-
-
-
-
-
-
-
-# Plot two districts that are near to each others one is treated and one is not 
-
-
-
-
-
-#Perform basic statistical inference 
